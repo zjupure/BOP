8bb6e2db49a63e902dcc612202c61bc76f593548
Echo Chen <echochen541@163.com>
2016-05-07 19:14:52 +0800

    initial REST service endpoint, url is http://localhost:8080/BOP/route/id1=15&id2=14

---
+++ BOP/src/com/bop/server/Route.java
@@ -0,0 +1,23 @@
+package com.bop.server;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+
+@Path("/route")
+public class Route {
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String sayHello() {
+		return "Hello World!";
+	}
+
+	@GET
+	@Path("/id1={id1}&id2={id2}")
+	@Produces("text/plain;charset=UTF-8")
+	public String sayHelloToUTF8(@PathParam("id1") String id1, @PathParam("id2") String id2) {
+		return "id1 = " + id1 + " id2 = " + id2;
+	}
+}
