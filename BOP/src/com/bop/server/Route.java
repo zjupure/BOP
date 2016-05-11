de0343000cc9443950c8ce599d80af29a3870cbe
Echo Chen <echochen541@163.com>
2016-05-11 23:46:47 +0800

    get id1&id2, initialize GraphSearch failed

--- BOP/src/com/bop/server/Route.java
+++ BOP/src/com/bop/server/Route.java
@@ -5,29 +5,26 @@ import java.util.concurrent.ExecutionException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.QueryParam;
 
 import com.bop.algorithm.GraphSearch;
 
 @Path("/route")
 public class Route {
 	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String sayHello() {
-		return "Hello World!";
-	}
-
-	@GET
-	@Path("/id1={id1}&id2={id2}")
-	@Produces("text/plain;charset=UTF-8")
-	public String handleGetRequest(@PathParam("id1") String id1, @PathParam("id2") String id2) {
+	@Produces("text/plain")
+	public String handleGet(@QueryParam("id1") long id1, @QueryParam("id2") long id2) {
 		System.out.println("id1 = " + id1 + " id2 = " + id2);
 		GraphSearch search = new GraphSearch();
 		String result = null;
+
 		try {
-			result = search.search(Long.parseLong(id1), Long.parseLong(id2));
-		} catch (NumberFormatException | InterruptedException | ExecutionException e) {
+			result = search.search(id1, id2);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return result;
