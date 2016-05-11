dd222c2f29ec114de84bb9c7a1870ee8d42fa02c
Echo Chen <echochen541@163.com>
2016-05-11 23:15:20 +0800

    get id1&id2, call graph search

--- BOP/src/com/bop/server/Route.java
+++ BOP/src/com/bop/server/Route.java
@@ -1,10 +1,14 @@
 package com.bop.server;
 
+import java.util.concurrent.ExecutionException;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
+
+import com.bop.algorithm.GraphSearch;
 
 @Path("/route")
 public class Route {
@@ -18,6 +22,14 @@ public class Route {
 	@Path("/id1={id1}&id2={id2}")
 	@Produces("text/plain;charset=UTF-8")
 	public String handleGetRequest(@PathParam("id1") String id1, @PathParam("id2") String id2) {
-		return "id1 = " + id1 + " id2 = " + id2;
+		System.out.println("id1 = " + id1 + " id2 = " + id2);
+		GraphSearch search = new GraphSearch();
+		String result = null;
+		try {
+			result = search.search(Long.parseLong(id1), Long.parseLong(id2));
+		} catch (NumberFormatException | InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 }
